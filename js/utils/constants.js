f572c1b6caa372bda435cdc3637ce6798ad2aa92
sussybocca <babyyodacutefry@gmail.com>
2026-02-24 12:04:55 -0600

    Create constants.js

---
+++ js/utils/constants.js
@@ -0,0 +1,14 @@
+// constants.js – default settings and constants
+export const constants = {
+    DEFAULT_SETTINGS: {
+        theme: 'dark',
+        glitchIntensity: 1,
+        bgEffect: 'perlin',
+        searchEngine: 'internal',
+        suggestionsEnabled: true,
+        historyEnabled: true,
+        analyticsEnabled: true,
+        maxTabs: 20,
+        homepage: 'newtab.html'
+    }
+};
